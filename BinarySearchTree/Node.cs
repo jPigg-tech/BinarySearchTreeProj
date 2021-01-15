720f79f05c8c42b58c63828e7eb42485c7fce6dc
unknown <jeffpigg5@gmail.com>
2021-01-15 14:48:20 -0600

    Created a new project, BinarySearchTree, added two clesses to the project, Node.cs and BinarySearchTree.cs

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        // represent the individual nodes that make up a binary search tree
+    }
+}
